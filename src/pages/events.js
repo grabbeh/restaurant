3262f06a5ea0d6294dd790110f83f64d08a27d7b
Michael <mbg@outlook.com>
2018-11-27 09:34:14 +0000

    Update events.js

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,1 +1,18 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import Box from '../components/Box'
+import Text from '../components/Text'
+
+const events = () => (
+  <Box>
+    <Layout>
+        <Header />
+        <Box p={3}>
+         <Text>Events!!!</Text>
+        </Box>
+    </Layout>
+  </Box>
+ )
+
 
