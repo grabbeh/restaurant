d92579f3b9c53b4afe5fbfcb36e1c147c46e7826
Michael <mbg@outlook.com>
2018-11-27 10:43:19 +0000

    Update navigation.js

--- src/pages/navigation.js
+++ src/pages/navigation.js
@@ -30,10 +30,10 @@ const menu = () => {
               {data.site.siteMetadata.menuTypes.map(({ link, title }) => {
                 return (
                   <Flex justifyContent='center'>
-                    <Box width={200} height={100} key={title} mr={4} pb={4}>
+                    <Box width={200} height={100} key={title} pb={4}>
                       <Link to={link}>
                         <Button bg='go-light-peach' width={1}>
-                          <Text fontSize={2} fontWeight='bold' color='petrol'>
+                          <Text fontSize={3} fontWeight='bold' color='petrol'>
                             {title}
                           </Text>
                         </Button>
