956887e17eb5474dcab9c63fc7df4cacec1467c9
Michael <mbg@outlook.com>
2018-11-27 09:35:12 +0000

    Update info.js

--- src/pages/info.js
+++ src/pages/info.js
@@ -1,1 +1,17 @@
 
+import React from 'react'
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import Box from '../components/Box'
+import Text from '../components/Text'
+
+const events = () => (
+  <Box>
+    <Layout>
+        <Header />
+        <Box p={3}>
+         <Text>Info!!!</Text>
+        </Box>
+    </Layout>
+  </Box>
+ )
