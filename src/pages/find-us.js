04074c2e96ff061dfc6a021a252dd87c6d34ee53
Michael <mbg@outlook.com>
2018-11-27 09:45:42 +0000

    Update find-us.js

--- src/pages/find-us.js
+++ src/pages/find-us.js
@@ -1,1 +1,18 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import Box from '../components/Box'
+import Text from '../components/Text'
 
+const find = () => (
+  <Box>
+    <Layout>
+        <Header />
+        <Box p={3}>
+         <Text>Find us!!!</Text>
+        </Box>
+    </Layout>
+  </Box>
+ )
+
+export default find
