0c30c704b877a18ef28300cce7e82a4dfd9df139
Michael <mbg@outlook.com>
2018-11-27 13:12:15 +0000

    Update menus.js

--- src/pages/menus.js
+++ src/pages/menus.js
@@ -9,46 +9,30 @@ import Header from '../components/Header'
 const evening = props => {
   const edges = props.data.allContentfulMenuItem.edges
   const title = edges[0].node.type
+  
+  let menuItems = groupBy(
+    props.data.allContentfulMenuItem.edges.map(i => {
+      return i.node
+    }),
+    'type'
+  )
+  
+  let { Brunch, Lunch, Drink, Dinner, Breakfast } = props 
+ 
   return (
     <Layout bg='go-light-peach'>
       <Header />
       <Box>
-        <Box>
-          <Flex justifyContent='center'>
-            <Box width={[1, 450]} zIndex={1}>
-              <Box mx={3} my={4}>
-                <Text fontSize={3} fontWeight='bold'>{title}</Text>
-                {edges.map(({ node: { name, price, description } }) => {
-                  return (
-                    <Box key={name}>
-                      <Flex flexWrap='wrap'>
-                        <Box width={0.8}>
-                          <Text pt={3} fontSize={2} fontWeight='bold'>
-                            {name}
-                          </Text>
-                          {description &&
-                            <Text
-                              pt={1}
-                              pb={2}
-                              dangerouslySetInnerHTML={{
-                                __html: description.childMarkdownRemark.html
-                              }}
-                            />}
-                        </Box>
-                        <Box width={0.1}>
-                          <Flex justifyContent='flex-end'>
-                            <Text lineHeight='1.7' pt={3}>{`£${price}`}</Text>
-                          </Flex>
-                        </Box>
-                      </Flex>
-                    </Box>
-                  )
-                })}
-              </Box>
-            </Box>
-          </Flex>
+        <Flex justifyContent='center'>
+         <Box width={[1, 450]} zIndex={1}>
+          <Menu title='Breakfast' items={Breakfast}/>
+          <Menu title='Brunch' items={Breakfast}/>
+          <Menu title='Lunch' items={Lunch}/>
+          <Menu title='Dinner' items={Dinner}/>
+          <Menu title='Drinks' items={Drink}/>
         </Box>
-      </Box>
+      </Flex>
+    </Box>
     </Layout>
   )
 }
@@ -57,7 +41,7 @@ export default evening
 
 export const query = graphql`
    {
-      allContentfulMenuItem(filter: { type: { eq: "Dinner" } })  {
+      allContentfulMenuItem {
         edges {
           node {
             name
@@ -74,3 +58,39 @@ export const query = graphql`
     }
   
 `
+
+const Menu = ({ items, title }) => {
+  return (
+    <Box mx={3} my={4}>
+      <Text fontSize={3} fontWeight='bold'>{title}</Text>
+        {items.map(({ name, price, description  }) => {
+          return (
+            <Box key={name}>
+              <Flex flexWrap='wrap'>
+                <Box width={0.8}>
+                  <Text pt={3} fontSize={2} fontWeight='bold'>
+                     {name}
+                   </Text>
+                    {description &&
+                    <Text
+                      pt={1}
+                      pb={2}
+                      dangerouslySetInnerHTML={{
+                                __html: description.childMarkdownRemark.html
+                      }}
+                     />}
+                    </Box>
+                    <Box width={0.1}>
+                      <Flex justifyContent='flex-end'>
+                        <Text lineHeight='1.7' pt={3}>{`£${price}`}</Text>
+                      </Flex>
+                     </Box>
+                   </Flex>
+                 </Box>
+                )
+             })}
+           </Box>
+        </Box>
+)
+
+}
