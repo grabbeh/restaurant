ec3f69bb1de0dd1a7494fe35bd8828623ddb1976
Michael <mbg@outlook.com>
2020-07-05 11:28:34 +0100

    Update ContactDetails.js

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -27,11 +27,15 @@ const ContactDetails = () => (
         HOURS
       </Text>
     </Box>
-    <Text fontSize={14}>Mon - Friday</Text>
-    <Text fontSize={14}>8am - 11pm</Text>
+    <Text fontSize={14}>Mon - Thursday</Text>
+    <Text fontSize={14}>9am - 8pm</Text>
     <Box mt={2}>
-      <Text fontSize={14}>Saturday & Sunday</Text>
+      <Text fontSize={14}>Friday & Saturday</Text>
       <Text fontSize={14}>9am - 11pm</Text>
+    </Box>
+      <Box mt={2}>
+      <Text fontSize={14}>Sunday</Text>
+      <Text fontSize={14}>9am - 8pm</Text>
     </Box>
   </Fragment>
 )
